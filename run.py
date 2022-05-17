0c0d0698828a76ef43537b2ea070177c66b14523
RoMcGill <ro.mcgill12@gmail.com>
2022-05-17 18:46:28 +0000

    up to make design your own on walkthrough video

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/carrers")
+def carrers():
+    return render_template("carrers.html")
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
